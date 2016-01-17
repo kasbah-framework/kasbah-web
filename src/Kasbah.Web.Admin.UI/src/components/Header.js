b3da9df186863138bcfa6e2306e0877a13ee9051
Brendan McKenzie <hello@brendanmckenzie.com>
2016-01-17 21:02:33 +1100

    Updating branding

--- src/Kasbah.Web.Admin.UI/src/components/Header.js
+++ src/Kasbah.Web.Admin.UI/src/components/Header.js
@@ -2,31 +2,32 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class extends React.Component {
-    render () {
-      return (
-            <nav className='navbar navbar-dark navbar-static-top header'>
-                <Link className='navbar-brand' to='/'>DASHBOARD</Link>
-                <ul className='nav navbar-nav'>
-                    <li className='nav-item'>
-                        <Link className='nav-link' to='/content'>Content</Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link className='nav-link' to='/media'>Media</Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link className='nav-link' to='/analytics'>Analytics</Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link className='nav-link' to='/users'>Users</Link>
-                    </li>
-                </ul>
+  render () {
+    return (
+      <nav className='navbar navbar-dark navbar-static-top header'>
+        <div className='container'>
+          <Link className='navbar-brand' to='/'><img src='/logo.png' height='30' style={{margin:0}} /></Link>
+          <ul className='nav navbar-nav'>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/content'>Content</Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/media'>Media</Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/analytics'>Analytics</Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/users'>Users</Link>
+            </li>
+          </ul>
 
-                <ul className='nav navbar-nav pull-right'>
-                    <li className='nav-item'>
-                        <button className='btn-link nav-link'>Logout</button>
-                    </li>
-                </ul>
-            </nav>
-        );
-    }
+          <ul className='nav navbar-nav pull-right'>
+            <li className='nav-item'>
+              <button className='btn-link nav-link'>Logout</button>
+            </li>
+          </ul>
+        </div>
+      </nav> );
+  }
 }
