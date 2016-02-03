d916ae8b6108dfd9ecadeac1c03aa6eac100d1ff
Brendan McKenzie <brendan.mckenzie@deepend.com.au>
2016-02-03 18:23:16 +1100

    Updated dist dir for UI

--- src/Kasbah.Web.Admin.UI/config/_base.js
+++ src/Kasbah.Web.Admin.UI/config/_base.js
@@ -12,7 +12,7 @@ const config = {
   // ----------------------------------
   path_base  : path.resolve(__dirname, '../'),
   dir_client : 'src',
-  dir_dist   : 'dist',
+  dir_dist   : '../Kasbah.Web.Admin/wwwroot',
   dir_server : 'server',
   dir_test   : 'tests',
 
