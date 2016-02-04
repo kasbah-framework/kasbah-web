d5924259eb7552048c8e293c5f608c3baa2bf555
Brendan McKenzie <brendan.mckenzie@deepend.com.au>
2016-02-04 16:30:43 +1100

    Updating media view prototype

--- src/Kasbah.Web.Admin.UI/src/views/MediaView.js
+++ src/Kasbah.Web.Admin.UI/src/views/MediaView.js
@@ -1,99 +1,43 @@
 import React from 'react';
 
-// TODO: this is all hacked together and is currently only meant
-// as a prototype
-
 export default class extends React.Component {
-    constructor () {
-      super();
+  constructor () {
+    super();
 
-      this.state = {};
-    }
+    this.state = {};
+  }
 
-    _renderUploadModal () {
-        // TODO: hook this up properly...
-      return (
-            <div>
-                <div className='modal-backdrop in' />
-                <div className='modal' style={{'display': 'block'}}>
-                    <div className='modal-dialog' role='document'>
-                        <div className='modal-content'>
-                            <div className='modal-header'>
-                                <button type='button' className='close' data-dismiss='modal' aria-label='Close' onClick={this.handleHideMediaUpload.bind(this)}>
-                                    <span aria-hidden='true'>&times;</span>
-                                    <span className='sr-only'>Close</span>
-                                </button>
-                                <h4 className='modal-title'>Media Upload</h4>
-                            </div>
-                            <div className='modal-body'>
-                                <form>
-                                    <fieldset className='form-group'>
-                                        <input type='file' className='form-control-file' ref='file' />
-                                    </fieldset>
-                                </form>
-                                <p>Show some details about the file to be uploaded, size, type, etc...</p>
-                                <p>Something for later... give implement a media editing tool (image cropper, etc...)</p>
-                                <p>Also allow for multiple file uploads</p>
-                            </div>
-                            <div className='modal-footer'>
-                                <button type='button' className='btn btn-sm btn-secondary' data-dismiss='modal' onClick={this.handleHideMediaUpload.bind(this)}>Cancel</button>
-                                <button type='button' className='btn btn-primary' onClick={this.handleHideMediaUpload.bind(this)}>Upload</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
-    handleShowMediaUpload () {
-      this.setState({ showMediaUpload: true });
-        // HACK!
-      document.body.className += 'modal-open';
-    }
-
-    handleHideMediaUpload () {
-      this.setState({ showMediaUpload: false });
-        // HACK!
-      document.body.className = document.body.className.replace('modal-open', '');
-    }
-
-    render () {
-      return (
-            <div className='container page-content'>
-                <ul className='breadcrumb'>
-                    <li>media root</li>
-                    <li>...</li>
-                </ul>
-                <div className='row header-options'>
-                    <div className='col-lg-6'>
-                        <button className='btn btn-sm btn-success' onClick={this.handleShowMediaUpload.bind(this)}>Upload media</button>
-                        <button className='btn btn-sm btn-secondary'>New folder</button>
-                    </div>
-                    <div className='col-lg-4 col-lg-offset-2'>
-                        <form>
-                            <input className='form-control form-control-sm' type='text' placeholder='Search' />
-                        </form>
-                    </div>
-                </div>
-
-                <div className='card-columns'>
-                {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((a, index) => (
-                        <div className='card' key={index}>
-                            <div className='card-header'>
-                                Name of the media
-                            </div>
-                            <div className='card-block'>
-                                <p>Media preview goes here</p>
-                            </div>
-                            <div className='card-footer'>
-                                <span>Action buttons go here</span>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                {this.state.showMediaUpload && this._renderUploadModal()}
-            </div>
-        );
-    }
+  render () {
+    return (
+        <div className='container'>
+          <table className='table is-striped'>
+            <thead>
+              <tr>
+                <th />
+                <th>File name</th>
+                <th style={{ width: 200 }}>Modified</th>
+                <th style={{ width: 120 }}>Type</th>
+                <th style={{ width: 120 }}>Size</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className='table-icon'><i className='fa fa-folder' /></td>
+                <td className='table-link'><a href=''>some folder</a></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td className='table-icon'><i className='fa fa-image' /></td>
+                <td className='table-link'><a href=''>some image</a></td>
+                <td>February 4, 2016 4:22pm</td>
+                <td>image/jpeg</td>
+                <td>74.2 kb</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+    );
+  }
 }
