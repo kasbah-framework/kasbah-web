cc9a702bdef8b52adfa26391196fd03741755855
Brendan McKenzie <hello@brendanmckenzie.com>
2016-02-05 08:01:22 +1100

    Updated version content base

--- src/Kasbah.Web/Models/VersionedContentBase.cs
+++ src/Kasbah.Web/Models/VersionedContentBase.cs
@@ -8,17 +8,22 @@ namespace Kasbah.Web.Models
         #region Public Properties
 
         public IEnumerable<object> Versions { get; set; }
-        public abstract Type VersionType { get; set; }
+
+        public abstract Type VersionType { get; }
 
         #endregion
 
         #region Public Methods
 
-        public ContentBase SelectVersion(object context)
-        {
-            return null;
-        }
+        public abstract ContentBase SelectVersion(object context);
 
         #endregion
+    }
+
+    public abstract class VersionedContentBase<T> : VersionedContentBase
+    {
+        public new IEnumerable<T> Versions { get; set; }
+
+        public override Type VersionType { get { return typeof(T); } }
     }
 }
