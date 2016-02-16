cf82f0c8f2b96a9351bc7b3342875c69d6fba55d
Brendan McKenzie <hello@brendanmckenzie.com>
2016-02-17 07:51:06 +1100

    Refactoring model definition generator

--- src/Kasbah.Web.Admin/Controllers/ContentController.cs
+++ src/Kasbah.Web.Admin/Controllers/ContentController.cs
@@ -154,27 +154,38 @@ namespace Kasbah.Web.Admin.Controllers
         {
             var nameResolver = new CamelCasePropertyNamesContractResolver();
 
+            var editableFields = type.GetRuntimeProperties()
+                .Select(ent => new {
+                    Alias = nameResolver.GetResolvedPropertyName(ent.Name),
+                    Field = ent,
+                    Type = ent.GetAttributeValue<EditorAttribute, string>(attr => attr?.Editor),
+                    Section = ent.GetAttributeValue<SectionAttribute, SectionAttribute>(attr => attr),
+                    Editor = ent.GetAttributeValue<EditorAttribute, EditorAttribute>(attr => attr),
+                    DisplayName = ent.GetAttributeValue<DisplayNameAttribute, DisplayNameAttribute>(attr => attr),
+                    Name = ent.Name
+                })
+                .Where(ent => ent.Type != null);
+
             return new ModelDefinition
             {
-                Sections = type.GetAllProperties()
-                    .Select(prop => prop.GetAttributeValue<SectionAttribute, SectionAttribute>(attr => attr))
+                Sections = editableFields
+                    .Select(ent => ent.Section)
                     .OrderBy(ent => ent?.SortOrder ?? int.MaxValue - 1)
                     .Select(ent => ent?.Section ?? "General")
                     .Distinct(),
-                Fields = type.GetAllProperties()
-                    .OrderBy(ent => ent.GetAttributeValue<EditorAttribute, int>(attr => attr?.SortOrder ?? int.MaxValue))
+                Fields = editableFields
+                    .OrderBy(ent => ent.Editor?.SortOrder ?? int.MaxValue)
                     .Select(prop =>
                     {
                         return new FieldDef
                         {
-                            Alias = nameResolver.GetResolvedPropertyName(prop.Name),
-                            DisplayName = prop.GetAttributeValue<DisplayNameAttribute, string>(attr => attr?.DisplayName) ?? prop.Name,
-                            HelpText = prop.GetAttributeValue<DisplayNameAttribute, string>(attr => attr?.HelpText),
-                            Section = prop.GetAttributeValue<SectionAttribute, string>(attr => attr?.Section) ?? "General",
-                            Type = prop.GetAttributeValue<EditorAttribute, string>(attr => attr?.Editor)
+                            Alias = prop.Alias,
+                            DisplayName = prop.DisplayName?.DisplayName ?? prop.Name,
+                            HelpText = prop.DisplayName?.HelpText,
+                            Section = prop.Section?.Section ?? "General",
+                            Type = prop.Type
                         };
                     })
-                    .Where(ent => ent.Type != null)
             };
         }
 
